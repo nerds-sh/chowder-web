783cde2fab42ee648c2fb4097cbd09e1cfd6f87b
TBiannca <bia.topircean@gmail.com>
2022-09-30 00:35:08 +0300

    [master] Fix pix

--- src/review/index.jsx
+++ src/review/index.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core'
+import {Box, Button, Grid, Typography} from '@material-ui/core'
 import { RadioGroupRating } from './reviews'
 import { useDispatch } from 'react-redux'
 import { useCallback } from 'react'
@@ -22,21 +22,25 @@ const useCreateReview = () => {
 
 const initialValues = {
     userName: null,
-    rate: 1,
+    rate: null,
     text: null
 }
 
 export const Review = () => {
     return <Formik initialValues={initialValues} onSubmit={useCreateReview()}>
       <Form>
-        <h2>Leave us your opinion.</h2>
-        <Typography>Add your name</Typography>
-        <Field type="text" name="userName" />
-        <Typography> What do you think about our restaurant ?</Typography>
-        <RadioGroupRating />
-        <Typography>What would you like to share with us ?</Typography>
-        <Field type="text" name="text" />
-        <button type="submit">Submit</button>
+        <h2 style={{ marginTop: 40 }}>Leave us your opinion.</h2>
+        <Box style={{ marginTop: 100 }}>
+            <Typography>Add your name</Typography>
+            <Field type="text" name="userName" style={{ width: '80%' }}/>
+            <Typography style={{ marginTop: 50 }}> What do you think about our restaurant ?</Typography>
+            <RadioGroupRating />
+            <Typography style={{ marginTop: 50 }}>What would you like to share with us ?</Typography>
+            <Field type="text" name="text" style={{ height: 150, width: '80%' }}/>
+            <Grid sm={12} xs={12} md={12} style={{ marginTop: 20 }}>
+            <Button type="submit" color="success">Submit</Button>
+            </Grid>
+        </Box>
       </Form>
     </Formik>
 }
